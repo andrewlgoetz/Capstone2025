038e13d45d0fe5a0332d8e3d9f51162126899ff7
Haley Johnson <haleyjohnson@Haleys-MacBook-Air.local>
2025-10-29 15:44:39 -0400

    oops

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql+psycopg2://myuser:mypassword@localhost:5432/inventory_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
