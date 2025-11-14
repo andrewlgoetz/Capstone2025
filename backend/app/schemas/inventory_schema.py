a7a5db0ad31709248704e765a120c77558842517
andrewlgoetz <100867543+andrewlgoetz@users.noreply.github.com>
2025-11-14 11:11:26 -0500

    cleanup schema

--- backend/app/schemas/inventory_schema.py
+++ backend/app/schemas/inventory_schema.py
@@ -2,15 +2,6 @@ from pydantic import BaseModel, Field, ConfigDict
 from datetime import date,datetime
 from typing import Optional,Literal
 
-# class InventoryBase(BaseModel):
-#     name: str
-#     category: Optional[str]
-#     barcode: Optional[str]
-#     quantity: int
-#     unit: Optional[str]
-#     expiration_date: Optional[date]
-#     location_id: Optional[int] # do we want to store this here or in another object, review the relationship with bankID
-
 class InventoryCreate(BaseModel):
     # item_id: int
     name: str
@@ -25,13 +16,6 @@ class ScanRequest(BaseModel):
     barcode: str
     quantity: int
 
-# old
-# class InventoryRead(InventoryBase):
-#     item_id: int
-#     date_added: Optional[date]
-
-#     class Config:
-#         orm_mode = True
 
 # TODO, this is the response model from a scan request 
 # what info related to a barcdoe do we want to maintain
